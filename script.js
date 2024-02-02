4690d726356a02dee386aeb1457ac4a901e98809
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-01 23:48:15 -0300

    Atualização de imagens

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function alerta(){
+    alert('Atenção! Esse site foi feito para ver em monitores e não em telas de celulares. Obrigado.')
+}
+
 function pica(){
     alert('Evandro esta doente, voce poderia ajudar ele?')
 }
