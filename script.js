47bcb8fc12a162b19d87baba6eefdb8da5d56729
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-04 03:28:56 -0300

    Atualizações das posições das imagens parte 1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 function alerta(){
     alert('Atenção! Esse site foi feito para ver em monitores e não em telas de celulares. Obrigado.')
 }
@@ -5,3 +6,4 @@ function alerta(){
 function pica(){
     alert('Evandro esta doente, voce poderia ajudar ele?')
 }
+*/
