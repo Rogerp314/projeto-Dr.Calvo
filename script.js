ca1607d413e0b37c8cdbf44ffa33c6e1e174ae11
Rogerp314 <138505724+Rogerp314@users.noreply.github.com>
2024-02-05 01:35:22 -0300

    Update

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-/*
+
 function alerta(){
     alert('Atenção! Esse site foi feito para ver em monitores e não em telas de celulares. Obrigado.')
 }
-
-function pica(){
-    alert('Evandro esta doente, voce poderia ajudar ele?')
-}
-*/
